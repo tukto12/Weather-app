0655008d75a7f30b58ad029da13e49c428d6573c
tukto <pawewoj214@gmail.com>
2023-04-10 15:05:04 +0200

    logic for 3 hour forecast

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 'use strict';
-
+// ===========================================================
+// variables to make API call
 const apiKey = 'secret';
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+const baseUrlHourly = 'http://api.openweathermap.org/data/2.5/forecast?';
 
 const city = document.querySelector('.city');
 const url = baseUrl + 'appid=' + apiKey + '&q=';
+const urlHourly = baseUrlHourly + 'appid=' + apiKey + '&q=';
+// ===========================================================
+
+// ===========================================================
+// variables to change the data on screen
+const windSpeed = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const pressure = document.querySelector('.pressure');
+const weatherDescription = document.querySelector('.weather-description');
+const temperature = document.querySelector('.temperature');
+const dateDay = document.querySelector('.date-day');
+const dateMonth = document.querySelector('.date-month');
+
+const todayArray = ['one', 'two', 'three', 'four'];
+// ===========================================================
 
 const btn = document.querySelector('.location-btn');
 
+// ===========================================================
+// arrays to get the name of month, day and to get the proper image
 const monthNames = [
 	'January',
 	'February',
@@ -33,21 +52,26 @@ const dayNames = [
 	'Sunday',
 ];
 
-const imgDescription = {
-	Thunderstorm: 'thunderstorm',
-	Drizzle: 'drizzle',
-	Rain: 'rain',
-	Snow: 'snow',
-	Clear: 'clear',
-	Clouds: 'clouds',
-};
+const imgDescription = [
+	'Thunderstorm',
+	'Drizzle',
+	'Rain',
+	'Snow',
+	'Clear',
+	'Clouds',
+	'Mist',
+];
+// ===========================================================
 
+// ===========================================================
+// get the acutall month, day
 let today = new Date();
 let day = String(dayNames[today.getDay()]);
 let mm = String(monthNames[today.getMonth()]);
 let dd = String(today.getDate());
 
 today = dd + ' ' + mm;
+// ===========================================================
 
 function convertTemp(temp) {
 	return Math.ceil(temp - 273.15);
@@ -57,37 +81,58 @@ function convertSpeed(speed) {
 	return Math.ceil((speed * 3600) / 1000);
 }
 
+function findTime(text) {
+	var timeRegex = /[0-2][0-9]:[0][0]/;
+
+	return text.match(timeRegex);
+}
+
+// function to check actuall weather
 async function checkWeather(city) {
 	const response = await fetch(url + city);
 	var data = await response.json();
 	try {
-		document.querySelector('.date-day').innerHTML = day + ', &nbsp;';
-		document.querySelector('.date-month').innerHTML = today;
-		document.querySelector('.temperature').innerHTML = convertTemp(
-			data.main.feels_like
-		);
-		document.querySelector('.weather-description').classList.remove('error');
-		document.querySelector('.weather-description').innerHTML =
-			data.weather[0].description;
-		document.querySelector('.wind').innerHTML =
-			convertSpeed(data.wind.speed) + ' km/h';
-		document.querySelector('.humidity').innerHTML = data.main.humidity + ' %';
-		document.querySelector('.pressure').innerHTML = data.main.pressure + ' hPa';
-		if (imgDescription.hasOwnProperty(data.weather[0].main)) {
+		dateDay.innerHTML = day + ', &nbsp;';
+		dateMonth.innerHTML = today;
+		temperature.innerHTML = convertTemp(data.main.feels_like);
+		weatherDescription.classList.remove('error');
+		weatherDescription.innerHTML = data.weather[0].description;
+		windSpeed.innerHTML = convertSpeed(data.wind.speed) + ' km/h';
+		humidity.innerHTML = data.main.humidity + ' %';
+		pressure.innerHTML = data.main.pressure + ' hPa';
+		if (imgDescription.includes(data.weather[0].main)) {
 			document.getElementById('image-id').src =
 				'img/app/' + data.weather[0].main + '.png';
 		}
-		// console.log(data);
 	} catch (err) {
-		document.querySelector('.weather-description').classList.add('error');
-		document.querySelector('.weather-description').innerHTML = 'Error';
+		weatherDescription.classList.add('error');
+		weatherDescription.innerHTML = 'Error';
+	}
+}
+
+// function to check 3 hour forecast
+async function checkWeatherHourly(city) {
+	const response = await fetch(urlHourly + city);
+	var data = await response.json();
+
+	try {
+		for (let i = 0; i < todayArray.length; i++) {
+			document.querySelector('.temp' + '-' + todayArray[i]).innerHTML =
+				convertTemp(data.list[i].main.temp) + '°';
+			document.querySelector('.time' + '-' + todayArray[i]).innerHTML =
+				findTime(data.list[i].dt_txt)[0];
+
+			if (imgDescription.includes(data.list[i].weather[0].main)) {
+				document.getElementById('image-id' + '-' + todayArray[i]).src =
+					'img/app/' + data.list[i].weather[0].main + '.png';
+			}
+		}
+	} catch (err) {
+		console.log(err);
 	}
 }
 
 btn.addEventListener('click', () => {
 	checkWeather(city.value);
+	checkWeatherHourly(city.value);
 });
-
-// TODO '
-// ADD CHANGING IMAGE DEPEND ON THE WEATHER DESCRIPTION
-//
